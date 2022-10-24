e12cb7baac42beb7afb8d0c902dd76e601805860
ctinsman <ctinsman@thegraysonschool.org>
2022-10-24 13:55:26 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+class Rational:
+
+    def __init__(self, numer, denom):
+        #TODO: Implement this!
+        pass
+
+    def __add__(self, other):
+        #TODO: Implement this!
+        pass
+
+    def __sub__(self, other):
+        # TODO: Implement this!
+        pass
+
+    def __mul__(self, other):
+        # TODO: Implement this!
+        pass
+
+    def __truediv__(self, other):
+        # TODO: Implement this!
+        pass
+
+    #TODO: you will need to put your numerator and denominator
+    #TODO: in lowest terms in multiple functions. Maybe it's a
+    #TODO: good idea to make that it's own function?
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
